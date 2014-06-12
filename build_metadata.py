0d946042f7ab5969422d7618a6ed72a9cb86297d
daniel evertsson <daniel.evertsson@umu.se>
2014-06-12 13:36:43 +0200

    Now build_metadata merges all metadata files created by the script

--- build_metadata.py
+++ build_metadata.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
 import json
+import os
 
 from subprocess import Popen, PIPE
 from saml2.sigver import get_xmlsec_binary
+import subprocess
 
-#
+
+class RunScript:
+    def runScript(self, command, working_directory=None):
+        try:
+            p = subprocess.Popen(command,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE,
+                                 cwd=working_directory)
+            while(True):
+                retcode = p.poll() #returns None while subprocess is running
+                if(retcode is not None):
+                    break
+            p_out = p.stdout.read()
+            p_err = p.stderr.read()
+            return (True, p_out, p_err)
+        except Exception as ex:
+            return (False, None, None)
+
 MAKE_METADATA = "make_metadata.py"
 # or if you have problem with your paths be more specific
 #MAKE_METADATA = "/usr/bin/make_metadata.py"
@@ -13,11 +32,13 @@ MAKE_METADATA = "make_metadata.py"
 XMLSEC = get_xmlsec_binary(["/opt/local/bin", "/usr/local/bin"])
 
 MDNS = '"urn:oasis:names:tc:SAML:2.0:metadata"'
-NFORMAT = "samltest.swamid.se%ssp.xml"
+NFORMAT = "130.239.200.133%ssp.xml"
 
 CNFS = [""]
 COMBOS = json.loads(open("build.json").read())
 CNFS.extend(COMBOS.keys())
+
+fname_list = []
 
 for cnf in CNFS:
 
@@ -39,4 +60,21 @@ for cnf in CNFS:
     f = open(NFORMAT % fname, "w")
     f.write(txt)
     f.close()
-    #print etxt
+    fname_list.append(NFORMAT % fname)
+
+
+fp = open("sp_conf", "w")
+len = len(fname_list)
+count = 1
+for file in fname_list:
+    fp.write("local " + file)
+    if count != len:
+        fp.write(os.linesep)
+    count += 1
+fp.close()
+
+ok, p_out, p_err = RunScript().runScript(["merge_metadata.py", "sp_conf"])
+
+fp = open("all_sp.xml", "w")
+fp.write(p_out)
+fp.close()
