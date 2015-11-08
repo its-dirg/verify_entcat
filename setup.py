068c03ee2ad3116182fa000df484e7ac1850da59
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-11-08 05:57:13 +0100

    Additions for Travis CI integration.

--- setup.py
+++ setup.py
@@ -1,52 +1,21 @@
-from distutils.core import setup
-
-HTDOCS = "htdocs"
-TEMPL = "templates"
-STAT = "static"
-TARGET = "idpproxy/metadata/files"
+from setuptools import setup, find_packages
 
 setup(
     name='verify_entcat',
-    version='0.1',
-    url='',
-    #license='LICENSE.txt',
-    author='Roland Hedberg, Hans Hoerberg',
-    author_email='roland.hedberg@umu.se, hans.horberg@umu.se',
-    description='Op to Saml proxy based on the projects pyoidc and pysaml.',
-        install_requires=[
+    version='0.2',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    author='Roland Hedberg, Hans Hoerberg, Rebecka Gulliksson',
+    author_email='roland.hedberg@umu.se, hans.horberg@umu.se, rebecka.gulliksson@umu.se',
+    description='SAML IdP test utility for entity categories.',
+    install_requires=[
         'pysaml2',
-        'mako'
     ],
-    license="Apache 2.0",
+    url='https://github.com/its-dirg/verify_entcat',
+    license="Apache License 2.0",
     classifiers=[
         "Development Status :: 0.1 - Beta",
         "License :: OSI Approved :: Apache Software License",
         "Topic :: Software Development :: Libraries :: Python Modules"
-    ],
-    data_files=[
-            ("%s" % HTDOCS, ["%s/check_result.mako" % HTDOCS,
-                             "%s/test.mako" % HTDOCS]),
-            ("%s" % STAT, ["%s/arrowDown.png" % STAT,
-                           "%s/arrowLeft.png" % STAT,
-                           "%s/arrowRight.png" % STAT,
-                           "%s/arrowUp.png" % STAT,
-                           "%s/jquery.min.1.9.1.js" % STAT,
-                           "%s/jquery.tinysort.charorder.min.js" % STAT,
-                           "%s/jquery.tinysort.min.js" % STAT,
-                           "%s/loader.gif" % STAT,
-                           "%s/robots.txt" % STAT,
-                           "%s/style.css" % STAT,
-                           "%s/SWAMID.png" % STAT]),
-            ("%s/bootstrap/css/" % STAT, ["%s/bootstrap/css/bootstrap.css" % STAT,
-                                          "%s/bootstrap/css/bootstrap.min.css" % STAT,
-                                          "%s/bootstrap/css/bootstrap-theme.css" % STAT,
-                                          "%s/bootstrap/css/bootstrap-theme.min.css" % STAT]),
-            ("%s/bootstrap/fonts/" % STAT, ["%s/bootstrap/fonts/glyphicons-halflings-regular.eot" % STAT,
-                                            "%s/bootstrap/fonts/glyphicons-halflings-regular.svg" % STAT,
-                                            "%s/bootstrap/fonts/glyphicons-halflings-regular.ttf" % STAT,
-                                            "%s/bootstrap/fonts/glyphicons-halflings-regular.woff" % STAT]),
-            ("%s/bootstrap/js/" % STAT, ["%s/bootstrap/js/bootstrap.js" % STAT,
-                                         "%s/bootstrap/js/bootstrap.min.js" % STAT])
-
-        ]
+    ]
 )
