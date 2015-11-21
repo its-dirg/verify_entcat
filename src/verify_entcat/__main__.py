f44b60c17c613405df0a7fcc037fdb330c410ee0
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-11-21 06:59:45 +0100

    Added metadata upload service.

--- src/verify_entcat/__main__.py
+++ src/verify_entcat/__main__.py
@@ -3,14 +3,14 @@ from wsgiref.simple_server import make_server
 from werkzeug.debug import DebuggedApplication
 from werkzeug.wsgi import SharedDataMiddleware
 
-from verify_entcat.wsgi import app
+from verify_entcat.wsgi import app as verify_entcat
 
-app.debug = True
-app.secret_key = "abcdef"
-app.wsgi_app = DebuggedApplication(SharedDataMiddleware(app.wsgi_app, {
+verify_entcat.app.debug = True
+verify_entcat.app.secret_key = "abcdef"
+verify_entcat.app.wsgi_app = DebuggedApplication(SharedDataMiddleware(verify_entcat.app.wsgi_app, {
     '/static': ('verify_entcat', 'site/static')
 }))
 
 print("Serving on port 8000...")
-httpd = make_server('', 8000, app)
+httpd = make_server('', 9000, verify_entcat)
 httpd.serve_forever()
--- src/verify_entcat/wsgi.py
+++ src/verify_entcat/wsgi.py
@@ -1,129 +1,31 @@
-import os
-import random
-import string
+import metadata_upload.wsgi
+from werkzeug.wsgi import DispatcherMiddleware
 
-import flask
-import yaml
-from beaker.middleware import SessionMiddleware
-from flask import Flask
-from flask.globals import g
-from flask.sessions import SessionInterface
-from flask.templating import render_template
-from verify_entcat.configure import build_test_list, create_service_providers
-from verify_entcat.result_db import ResultDB
-from verify_entcat.saml import SSO, ACS, DS, RequestCache, ATTRIBUTE_RELEASE_POLICY
+from verify_entcat.service import app as verify_entcat
 
 
-class BeakerSessionInterface(SessionInterface):
-    def open_session(self, app, request):
-        session = request.environ['beaker.session']
-        return session
+class FlaskDispatcherMiddleware:
+    def __init__(self, app, mounts=None):
+        self.app = app
+        self.mounts = mounts or {}
 
-    def save_session(self, app, session, response):
-        session.save()
-
-def random_string(n=16):
-    return ''.join(
-        random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(n))
-
-
-def read_config():
-    config_file = os.environ.get('VERIFY_ENTCAT_CONFIG', 'config.yml')
-    with open(config_file) as f:
-        config = yaml.safe_load(f.read())
-
-    return config
-
-
-config = read_config()
-
-app = Flask(__name__)
-app.config.update(dict(
-    TESTS=build_test_list(config["available_tests"], config["test_text_descriptions"],
-                          ATTRIBUTE_RELEASE_POLICY),
-    SP=create_service_providers(config["available_tests"], config["verify_entcat_conf"]),
-    DISCOVERY_SERVICE=config["verify_entcat_conf"]["discovery_service"],
-    SECRET_KEY=config["verify_entcat_conf"].get("secret_key", random_string()),
-    RESULT_DB=config["verify_entcat_conf"]["result_db"]
-))
-
-session_opts = {
-    # TODO can't be server-side due to pyff redirecting to disco endpoint twice
-    "session.type": "cookie",
-    "session.validate_key": app.config["SECRET_KEY"]
-}
-
-app.wsgi_app = SessionMiddleware(app.wsgi_app, session_opts)
-app.session_interface = BeakerSessionInterface()
+    def __call__(self, environ, start_response):
+        script = environ.get('PATH_INFO', '')
+        path_info = ''
+        while '/' in script:
+            if script in self.mounts:
+                app = self.mounts[script]
+                break
+            script, last_item = script.rsplit('/', 1)
+            path_info = '/%s%s' % (last_item, path_info)
+        else:
+            app = self.mounts.get(script, self.app)
+        original_script_name = environ.get('SCRIPT_NAME', '')
+        environ['SCRIPT_NAME'] = original_script_name + script
+        environ['PATH_INFO'] = path_info
+        return app(environ, start_response)
 
 
-def get_db():
-    if not hasattr(g, 'result_db'):
-        g.result_db = ResultDB(app.config["RESULT_DB"])
-    return g.result_db
-
-
-@app.route("/")
-def index():
-    test_results = flask.session.get("test_results", {})
-    return render_template("test_list.html", tests=app.config['TESTS'], test_results=test_results)
-
-
-@app.route("/tests/<test_id>")
-def run_test(test_id):
-    if "request_cache" not in flask.session:
-        flask.session["request_cache"] = RequestCache()
-
-    request_origin = flask.request.url
-    redirect = DS(flask.session["request_cache"]).redirect_to_discovery_service(
-        app.config['SP'][test_id], app.config['DISCOVERY_SERVICE'], request_origin)
-
-    return redirect
-
-
-@app.route("/<test_id>/disco")
-def disco(test_id):
-    # TODO store selected IdP in session and don't redirect to discovery service every time?
-
-    idp_entity_id, request_origin = DS(flask.session["request_cache"]).parse_discovery_response(
-        flask.request.args)
-
-    authn_req = SSO(flask.session["request_cache"]).make_authn_request(
-        app.config['SP'][test_id], idp_entity_id, request_origin)
-    return authn_req
-
-
-@app.route("/<test_id>/acs/post", methods=["POST"])
-def acs(test_id):
-    authn_response = flask.request.form["SAMLResponse"]
-
-    idp_entity_id, test_result = ACS(flask.session["request_cache"]).parse_authn_response(
-        app.config['SP'][test_id], authn_response)
-
-    if "test_results" not in flask.session:
-        flask.session["test_results"] = {}
-    flask.session["test_results"][test_id] = test_result
-
-    db = get_db()
-    test_result.test_id = test_id
-    db[idp_entity_id] = test_result
-
-    return render_template("test_list.html", tests=app.config["TESTS"],
-                           test_results=flask.session["test_results"])
-
-
-@app.route("/results_overview")
-def results_overview():
-    db = get_db()
-
-    results_overview = {}
-
-    for idp_entity_id in db:
-        if idp_entity_id not in results_overview:
-            results_overview[idp_entity_id] = {}
-
-        for result_entry in db[idp_entity_id]:
-            results_overview[idp_entity_id][result_entry.test_id] = result_entry
-
-    return render_template("results_overview.html", tests=app.config["TESTS"],
-                           results_overview=results_overview)
+app = DispatcherMiddleware(verify_entcat, {
+    "/upload_metadata": metadata_upload.wsgi.app
+})
