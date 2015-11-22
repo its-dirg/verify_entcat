e0f24fd916b379271b7938ce751d8c2e4260cecb
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-11-22 14:15:51 +0100

    Removed unused code.

--- src/verify_entcat/wsgi.py
+++ src/verify_entcat/wsgi.py
@@ -3,29 +3,6 @@ from werkzeug.wsgi import DispatcherMiddleware
 
 from verify_entcat.service import app as verify_entcat
 
-
-class FlaskDispatcherMiddleware:
-    def __init__(self, app, mounts=None):
-        self.app = app
-        self.mounts = mounts or {}
-
-    def __call__(self, environ, start_response):
-        script = environ.get('PATH_INFO', '')
-        path_info = ''
-        while '/' in script:
-            if script in self.mounts:
-                app = self.mounts[script]
-                break
-            script, last_item = script.rsplit('/', 1)
-            path_info = '/%s%s' % (last_item, path_info)
-        else:
-            app = self.mounts.get(script, self.app)
-        original_script_name = environ.get('SCRIPT_NAME', '')
-        environ['SCRIPT_NAME'] = original_script_name + script
-        environ['PATH_INFO'] = path_info
-        return app(environ, start_response)
-
-
 app = DispatcherMiddleware(verify_entcat, {
     "/upload_metadata": metadata_upload.wsgi.app
 })
